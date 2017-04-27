270dea885c0dc23074dd7d268c00c67ea0356dac
Luis Almaguer <imaximuz@gmail.com>
2017-04-27 17:03:48 -0500

    Se agregó el modal para el log in y el registro

--- public/js/ezpc.js
+++ public/js/ezpc.js
@@ -2,6 +2,18 @@
 var page = {
     ready: function(){
         $(window).scroll(page.onScrollDown);
+        $("#tabRegistrar, #tabIniciar").on("click", page.onModalTabClick);
+        $("input").on("input", page.onInputKeyDown);
+        $("input").focusout(page.onInputFocusOut);
+        $('#lnkLogIn').click(page.onLogInClick);
+        $('#lnkSignIn').click(page.onSignInClick);
+        $('#logInModal').on('show.bs.modal', page.onModalShow);
+    },
+    onLogInClick: function(){
+        
+    },
+    onSignInClick: function(){
+
     },
     onScrollDown: function(){
         var scroll = $(window).scrollTop();
@@ -12,10 +24,53 @@ var page = {
             navBars.removeClass("navbar-absolute");
             navBars.addClass("navbar-fixed-top");
             logIn.hide();
-        }else{
+        } else {
             navBars.addClass("navbar-absolute");
             navBars.removeClass("navbar-fixed-top");
             logIn.show();
+        }
+    },
+    onModalShow: function(event){
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var tabIndex = button.data('tab'); // Extract info from data-* attributes
+        var modal = $(this);
+
+        if( tabIndex == 0){
+            $("#tabRegistrar").addClass('form-header-active');
+            $("#frmRegistrar").show();
+            $("#tabIniciar").removeClass('form-header-active');
+            $("#frmIniciar").hide();
+        }
+        else if(tabIndex == 1){
+            $("#tabRegistrar").removeClass('form-header-active');
+            $("#frmRegistrar").hide();
+            $("#tabIniciar").addClass('form-header-active');
+            $("#frmIniciar").show();
+        }
+
+        console.log(tabIndex);
+    },
+    onModalTabClick: function(){
+        $("#frmRegistrar, #frmIniciar").slideToggle('fast');
+        $("#tabRegistrar, #tabIniciar").toggleClass('form-header-active');
+    },
+    onInputKeyDown: function(){
+        var label = $(this).parent('div.form-group').find('label.form-label');
+        if ($(this).val() === "" && !label.hasClass('hidden-label')) {
+            label.addClass('hidden-label');
+            label.slideDown('200');
+        }
+        if ($(this).val() !== "" && label.hasClass('hidden-label')) {
+            label.slideDown('200');
+            label.removeClass('hidden-label')
+        }
+    },
+    onInputFocusOut: function() {
+        var label = $(this).parent('div.form-group').find('label.form-label');
+        if ($(this).val() !== "") {
+            label.addClass('visible-label');
+        } else {
+            label.removeClass('visible-label');
         }
     }
 };
