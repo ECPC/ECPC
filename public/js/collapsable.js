e9992c99033ea489c715ddc917a36d1941844dc3
YamilYanez <yamilenriqueyanezmorales@hotmail.com>
2017-05-07 16:23:42 -0500

    Carrito Y UCompras
    + css tambien

--- public/js/collapsable.js
+++ public/js/collapsable.js
@@ -1,3 +1,57 @@
+
+    // var chart_config = {
+    //     chart: {
+    //         container: "#collapsable-example",
+
+    //         animateOnInit: true,
+            
+    //         node: {
+    //             collapsable: true
+    //         },
+    //         animation: {
+    //             nodeAnimation: "easeOutBounce",
+    //             nodeSpeed: 700,
+    //             connectorsAnimation: "bounce",
+    //             connectorsSpeed: 700
+    //         }
+    //     },
+    //     nodeStructure: {
+    //         image: "images/user-icon-dark.png",
+    //         children: [
+    //             {
+    //                 image: "images/user-icon-dark.png",
+    //                 collapsed: true,
+    //                 children: [
+    //                     {
+    //                         image: "images/user-icon-dark.png"
+    //                     }
+    //                 ]
+    //             },
+    //             {
+    //                 image: "images/user-icon-dark.png",
+    //                 childrenDropLevel: 1,
+    //                 children: [
+    //                     {
+    //                         image: "images/user-icon-dark.png"
+    //                     }
+    //                 ]
+    //             },
+    //             {
+    //                 pseudo: true,
+    //                 children: [
+    //                     {
+    //                         image: "images/user-icon-dark.png"
+    //                     },
+    //                     {
+    //                         image: "images/user-icon-dark.png"
+    //                     }
+    //                 ]
+    //             }
+    //         ]
+    //     }
+    // };
+
+ // Array approach
 var config = {
     container: "#collapsable-example",
 
@@ -49,3 +103,4 @@ pam = {
 },
 
 chart_config = [config, malory, lana, figgs, sterling, woodhouse, pam, cheryl];
+
