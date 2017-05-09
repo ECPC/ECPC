b8e98de1347c8b9e54ed6410a6b0172d7fdbbf5f
PanicJoker03 <buddynod32@hotmail.com>
2017-05-08 20:10:20 -0500

    plantillas de correo con estilo de luis

--- resources/views/activationEmail.blade.php
+++ resources/views/activationEmail.blade.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Activación de cuenta</title>
+	<title>Bienvenido a EZPC</title>
 </head>
 <body>
-	<a target="_blank" href="http://localhost:8000/api/user/{{$user->id}}/activateAccount/{{$user->activation_token}}">
-		Click aquí para activar tu cuenta
-	</a>
+    <table style="width:100%; font-family: sans-serif;">
+        <thead></thead>
+        <tbody>
+            <tr>
+                <td style="text-align:center;">
+                    <img src="https://preview.ibb.co/cSEET5/dark_logo.png" style="width: 200px" alt="EZPC">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h2>¡Hola!</h2>
+                    <p>
+                        Te damos la bienvenida a EZPC, la red de hardware más grande de Latino América.
+                    </p>
+                    <p>
+                        Estamos muy felices de que formes parte del equipo y que comiences a crear tu red y negocio lo antes posible.
+                    </p>
+                    <p>
+                        Estas a un paso más de tener acceso inmediato a tu oficina virtual, solo necesitas hacer clic en el siguiente enlace para terminar con el proceso de registro:
+                    </p>
+
+                    <p><a target="_blank" href="http://ezpc/api/user/{{$user->id}}/activateAccount/{{$user->activation_token}}">Haz clic aquí para completar el registro.</a></p>
+
+                    <p>
+                        ¡Buena suerte!
+                        <br>
+                        - EZPC
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
--- resources/views/subscriptionEmail.blade.php
+++ resources/views/subscriptionEmail.blade.php
@@ -1,9 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Subscripción a EZPC</title>
+	<title>Bienvenido a EZPC</title>
 </head>
 <body>
-	<p>Contraseña: {{$password}}</p>
+    <table style="width:100%; font-family: sans-serif;">
+        <thead></thead>
+        <tbody>
+            <tr>
+                <td style="text-align:center;">
+                    <img src="https://preview.ibb.co/cSEET5/dark_logo.png" style="width: 200px" alt="EZPC">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h2>¡Hola!</h2>
+                    <p>
+                        Te damos la bienvenida a EZPC, la red de hardware más grande de Latino América.
+                    </p>
+                    <p>
+                        Estamos muy felices de que formes parte del equipo y que comiences a crear tu red y negocio lo antes posible.
+                    </p>
+                    <p>
+                        Haz sido invitado por {{$parent->name}} para formar parte de EZPC, usa tu dirección de correo eléctronico y la siguiente contraseña para iniciar sesión.
+                    </p>
+					<p>Contraseña: {{$password}}</p>
+                    <p>
+                        ¡Buena suerte!
+                        <br>
+                        - EZPC
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
